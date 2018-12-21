95718a969ae32d0396af7d2f5866405177e70fef
MajdAbdalla <majd-3@hotmail.com>
2018-12-21 10:42:39 +0100

    Add Modes xml, make the quality of buttomImages better and change the font style in scores xml

---
+++ EzPzMath Project/app/src/main/java/com/waki/ezpzmath/ModesActivity.java
@@ -0,0 +1,13 @@
+package com.waki.ezpzmath;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modes);
+    }
+}
