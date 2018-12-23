e42aac555c28d72bb83e1adca182f3c6d7f20917
Zakuto <zakuto3@gmail.com>
2018-12-23 11:22:35 +0100

    Welcome msg after login

--- EzPzMath Project/app/src/main/java/com/waki/ezpzmath/ModesActivity.java
+++ EzPzMath Project/app/src/main/java/com/waki/ezpzmath/ModesActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.view.ViewDebug;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ModesActivity extends AppCompatActivity {
     private Button first_mode_button;
@@ -14,6 +18,7 @@ public class ModesActivity extends AppCompatActivity {
     private Button third_mode_button;
     private ImageButton score_button;
     private ImageButton Settings_button;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +65,7 @@ public class ModesActivity extends AppCompatActivity {
                 openSettingsActivity();
             }
         });
+        Toast.makeText(this,"Welcome " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
     }
 
     public void openLevelsActivity(){
